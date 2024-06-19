7c20893cd69ae160369985feb8ae3b8ddaaa6748
Jaime-Linares <jailinbar@alum.us.es>
2024-06-19 20:19:14 +0200

    [fix] changes in the API's call

--- src/App.js
+++ src/App.js
@@ -1,162 +1,71 @@
 import "./App.css";
-import Select from "react-select";
 import React, { useState, useEffect } from "react";
-import Card from "./SummaryCard";
-import { Line } from "react-chartjs-2";
-import Chart from "chart.js/auto";
+import Select from "react-select";
 
 
+export default function App() {
+  const baseUrl = "https://disease.sh/v3/covid-19";
+  const [activeLocation, setActiveLocation] = useState("CAN");
+  const [timesUpdated, setTimesUpdated] = useState("");
 
-function App() {
   const locationList = [
-    { value: "AB", label: "Alberta" },
-    { value: "BC", label: "British Columbia" },
-    { value: "canada", label: "Canada" },
-    { value: "MB", label: "Manitoba" },
-    { value: "NB", label: "New Brunswick" },
-    { value: "NL", label: "Newfoundland and Labrador" },
-    { value: "NT", label: "Northwest Territories" },
-    { value: "NS", label: "Nova Scotia" },
-    { value: "NU", label: "Nunavut" },
-    { value: "ON", label: "Ontario" },
-    { value: "PE", label: "Prince Edward Island" },
-    { value: "QC", label: "Quebec" },
-    { value: "SK", label: "Saskatchewan" },
-    { value: "YT", label: "Yukon" },
+    { value: "CAN", label: "Canada" },
+    { value: "USA", label: "United States" },
+    { value: "MEX", label: "Mexico" },
+    { value: "GBR", label: "United Kingdom" },
+    { value: "FRA", label: "France" },
+    { value: "DEU", label: "Germany" },
+    { value: "ESP", label: "Spain" },
+    { value: "ITA", label: "Italy" },
+    { value: "RUS", label: "Russia" },
+    { value: "BRA", label: "Brazil" },
+    { value: "IND", label: "India" },
+    { value: "CHN", label: "China" },
+    { value: "JPN", label: "Japan" },
+    { value: "AUS", label: "Australia" },
+    { value: "ZAF", label: "South Africa" }
   ];
-  const baseUrl = "https://api.opencovid.ca";
-  const timeseriesOptions = {
-    responsive: true,
-    normalized: true,
-    plugins: {
-      tooltip: {
-        enabled: false,
-      },
-    },
-    maintainAspectRatio: false,
-    scales: {
-      y: {
-        min: 0,
-      },
-    },
-  };
 
-  const [activeLocation, setActiveLocation] = useState("canada");
-  const [lastUpdated, setlastUpdated] = useState("");
-  const [summaryData, setSummaryData] = useState({});
-  const [timeseriesData, setTimeseriesData] = useState({ datasets: [], });
-
-
+  
   useEffect(() => {
     getVersion();
-    getSummaryData();
-    getTimeseriesData();
   }, [activeLocation]);
 
-
   const getVersion = async () => {
-    const res = await fetch(`${baseUrl}/version`);
+    const country = locationList.find(options => options.value === activeLocation).label;
+    const res = await fetch(`${baseUrl}/countries/${country}`);
     const data = await res.json();
-    setlastUpdated(data.version);
+    setTimesUpdated(data.updated);
   };
-
-  const getSummaryData = async () => {
-    setSummaryData({});
-    let res = await fetch(`${baseUrl}/summary?loc=${activeLocation}`);
-    let resData = await res.json();
-    if (resData.summary && resData.summary.length > 0) {
-      let summaryData = resData.summary[0];
-      let formattedData = {};
-      Object.keys(summaryData).map(
-        (key) => (formattedData[key] = summaryData[key].toLocaleString())
-      );
-      setSummaryData(formattedData);
-    }
-  };
-
-
-  const getTimeseriesData = async (location) => {
-    const res = await fetch(
-      `${baseUrl}/timeseries?loc=${activeLocation}&ymd=true`
-    );
-    const data = await res.json();
-    setTimeseriesData({ datasets: timeseriesDataMap(data) });
-  };
-
-  function timeseriesDataMap(fetchedData) {
-    let tsKeyMap = [
-      {
-        datasetLabel: "active",
-        dataKey: "active_cases",
-        dateKey: "date_active",
-        borderColor: "red",
-      },
-      {
-        datasetLabel: "mortality",
-        dataKey: "deaths",
-        dateKey: "date_death_report",
-        borderColor: "grey",
-      },
-      {
-        datasetLabel: "recovered",
-        dataKey: "recovered",
-        dateKey: "date_recovered",
-        borderColor: "blue",
-      },
-    ];
-
-    let datasets = [];
-    tsKeyMap.forEach((dataSeries) => {
-      if (fetchedData[dataSeries.datasetLabel]) {
-        let dataset = {
-          label: dataSeries.datasetLabel,
-          borderColor: dataSeries.borderColor,
-          data: fetchedData[dataSeries.datasetLabel].map((dataPoint) => {
-            return {
-              y: dataPoint[dataSeries.dataKey],
-              x: dataPoint[dataSeries.dateKey],
-            };
-          }),
-        };
-        datasets.push(dataset);
-      }
-    });
-    return datasets;
-  }
 
 
   return (
     <div className="App">
       <h1>COVID 19 Dashboard</h1>
-
       <div className="dashboard-container">
         <div className="dashboard-menu ">
           <Select className="dashboard-select"
             options={locationList}
-            onChange={(selectedOption) => setActiveLocation(selectedOption.value)}
-            defaultValue={locationList.filter((options) => options.value == activeLocation)}
+            onChange={(selectedOption) =>
+              setActiveLocation(selectedOption.value)
+            }
+            defaultValue={locationList.find(
+              options => options.value === activeLocation
+            )}
           />
-          <p className="update-date">Last Updated : {lastUpdated}</p>
+          <p className="update-date">
+            Times Updated : {timesUpdated}
+          </p>
         </div>
+      </div>
 
-        <div className="dashboard-timeseries">
-          <Line className="line-chart"
-            data={timeseriesData}
-            options={timeseriesOptions}
-          />
-        </div>
+      <div className="dashboard-timeseries">
 
-        <div className="dashboard-summary">
-          <Card title="Total Cases" value={summaryData.cumulative_cases} />
-          <Card title="Total Recovered" value={summaryData.cumulative_recovered}
-          />
-          <Card title="Total Deaths" value={summaryData.cumulative_deaths} />
-          <Card title="Total Vaccinated" value={summaryData.cumulative_avaccine} />
-        </div>
+      </div>
+
+      <div className="dashboard-summary">
+
       </div>
     </div>
   );
 }
-
-
-export default App;
