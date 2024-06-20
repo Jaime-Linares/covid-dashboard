9cae90759ed31c0d65cae4b8a49c6efa163ed18b
Jaime-Linares <jailinbar@alum.us.es>
2024-06-20 16:45:40 +0200

    [fix] dashboard summary done correctly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import Select from "react-select";
+import Card from "./SummaryCard";
 
 
 export default function App() {
   const baseUrl = "https://disease.sh/v3/covid-19";
   const [activeLocation, setActiveLocation] = useState("CAN");
   const [timesUpdated, setTimesUpdated] = useState("");
+  const [summaryData, setSummaryData] = useState({});
 
   const locationList = [
     { value: "CAN", label: "Canada" },
@@ -26,37 +28,45 @@ export default function App() {
     { value: "ZAF", label: "South Africa" }
   ];
 
-  
+
   useEffect(() => {
-    getVersion();
+    getCountryCovidInformation();
   }, [activeLocation]);
 
-  const getVersion = async () => {
+  const getCountryCovidInformation = async () => {
     const country = locationList.find(options => options.value === activeLocation).label;
     const res = await fetch(`${baseUrl}/countries/${country}`);
     const data = await res.json();
     setTimesUpdated(data.updated);
+    const summaryDataObject = {
+      cases: data.cases,
+      recovered: data.recovered,
+      deaths: data.deaths,
+      critical: data.critical
+    };
+    setSummaryData(summaryDataObject);
   };
 
 
-  return (
-    <div className="App">
-      <h1>COVID 19 Dashboard</h1>
-      <div className="dashboard-container">
-        <div className="dashboard-menu ">
-          <Select className="dashboard-select"
-            options={locationList}
-            onChange={(selectedOption) =>
-              setActiveLocation(selectedOption.value)
-            }
-            defaultValue={locationList.find(
-              options => options.value === activeLocation
-            )}
-          />
-          <p className="update-date">
-            Times Updated : {timesUpdated}
-          </p>
-        </div>
+return (
+  <div className="App">
+    <h1>COVID 19 Dashboard</h1>
+
+    <div className="dashboard-container">
+      <div className="dashboard-menu ">
+        <Select className="dashboard-select"
+          options={locationList}
+          onChange={(selectedOption) =>
+            setActiveLocation(selectedOption.value)
+          }
+          defaultValue={locationList.find(
+            options => options.value === activeLocation
+          )}
+        />
+
+        <p className="update-date">
+          Times Updated : {timesUpdated}
+        </p>
       </div>
 
       <div className="dashboard-timeseries">
@@ -64,8 +74,25 @@ export default function App() {
       </div>
 
       <div className="dashboard-summary">
-
+        <Card
+          title="Total Cases"
+          value={summaryData.cases}
+        />
+        <Card
+          title="Total Recovered"
+          value={summaryData.recovered}
+        />
+        <Card
+          title="Total Deaths"
+          value={summaryData.deaths}
+        />
+        <Card
+          title="Critical Cases"
+          value={summaryData.critical}
+        />
       </div>
     </div>
-  );
+
+  </div>
+);
 }
